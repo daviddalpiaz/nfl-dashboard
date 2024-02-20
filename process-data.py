bf13b91ebc82db88c898419ef2ec4be29961d07d
David Dalpiaz <dalpiaz2@illinois.edu>
2024-02-19 20:49:43 -0600

    fix copy versus view issue, remove unused imports

--- process-data.py
+++ process-data.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-# from lets_plot import *
-from utils import glimpse
 
 # get data
 url = "https://www.pro-football-reference.com/years/2023/games.htm"
@@ -21,8 +19,8 @@ nfl_winners = nfl_games[["Week", "Winner/tie"]]
 nfl_losers = nfl_games[["Week", "Loser/tie"]]
 
 # rename columns
-nfl_winners = nfl_winners.rename(columns={'Week': 'week', 'Winner/tie': 'team'})
-nfl_losers = nfl_losers.rename(columns={'Week': 'week', 'Loser/tie': 'team'})
+nfl_winners = nfl_winners.rename(columns={"Week": "week", "Winner/tie": "team"})
+nfl_losers = nfl_losers.rename(columns={"Week": "week", "Loser/tie": "team"})
 
 # add outcome variable 1 = win, -1 = lose
 nfl_winners["outcome"] = 1
@@ -32,17 +30,17 @@ nfl_losers["outcome"] = -1
 nfl = pd.concat([nfl_winners, nfl_losers], ignore_index=True)
 
 # define weeks that are not the regular season
-playoff_rounds = ['ConfChamp', 'Division', 'SuperBowl', 'Week', 'WildCard']
+playoff_rounds = ["ConfChamp", "Division", "SuperBowl", "Week", "WildCard"]
 
 # subset to regular season games
-nfl_regular = nfl[~nfl['week'].isin(playoff_rounds)]
+nfl_regular = nfl[~nfl["week"].isin(playoff_rounds)]
 
 # coerce week variable to be int
-nfl_regular["week"] = nfl_regular["week"].astype(int)
+nfl_regular.loc[:, "week"] = nfl_regular["week"].astype(int)
 
 # create weekly win-loss differential
-nfl_regular = nfl_regular.sort_values(['team', 'week'])
-nfl_regular['differential'] = nfl_regular.groupby('team')['outcome'].cumsum()
+nfl_regular = nfl_regular.sort_values(["team", "week"])
+nfl_regular["differential"] = nfl_regular.groupby("team")["outcome"].cumsum()
 nfl_regular = nfl_regular.drop("outcome", axis=1)
 
 # view full data
@@ -53,8 +51,8 @@ nfl_regular[nfl_regular["team"] == "San Francisco 49ers"]
 
 # plot data
 plt.figure(figsize=(10, 6))
-sns.lineplot(x='week', y='differential', hue='team', data=nfl_regular)
-plt.title('Differential vs Week by Team')
+sns.lineplot(x="week", y="differential", hue="team", data=nfl_regular)
+plt.title("Differential vs Week by Team")
 plt.show()
 
 # TODO: add "week 0"
